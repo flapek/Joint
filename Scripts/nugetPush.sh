d61bf0825ac3b692f469cddf684d566d6f9b2f0d
flapek <filap4@gmail.com>
2020-08-27 11:16:50 +0200

    add new scripts, update old

--- Scripts/nugetPush.sh
+++ Scripts/nugetPush.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-if $TRAVIS_BRANCH = master then
+if $TRAVIS_BRANCH = master 
+then
     dotnet nuget push Packages/SCL.*.nupkg --api-key $TRAVIS_NUGET_API_KEY --source https://api.nuget.org/v3/index.json
 fi
--- Scripts/pack.sh
+++ Scripts/pack.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-if $TRAVIS_BRANCH = master
-then
-    dotnet pack -c Release -o Packages --version-suffix 0.0.$TRAVIS_BUILD_NUMBER
-fi
+case "$TRAVIS_BRANCH" in
+  "master")
+  for dir in src/*/
+  do
+      dir=${dir%*/}
+      echo Publishing NuGet package:  ${dir##*/}
+      exec ./$dir/Scripts/dotnet-pack.sh &
+      wait
+  done
+
+  echo Finished publishing NuGet packages.
+  ;;
+esac
