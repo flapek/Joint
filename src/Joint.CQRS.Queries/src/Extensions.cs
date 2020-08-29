ed478ff6f4a5134d654b2a33edea0feeda134893
flapek <filap4@gmail.com>
2020-08-29 22:50:39 +0200

    add cqrs queries project

---
+++ src/Joint.CQRS.Queries/src/Extensions.cs
@@ -0,0 +1,26 @@
+using System;
+using Joint.CQRS.Queries.Dispatchers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Joint.CQRS.Queries
+{
+    public static class Extensions
+    {
+        public static IJointBuilder AddQueryHandlers(this IJointBuilder builder)
+        {
+            builder.Services.Scan(s =>
+                s.FromAssemblies(AppDomain.CurrentDomain.GetAssemblies())
+                    .AddClasses(c => c.AssignableTo(typeof(IQueryHandler<,>)))
+                    .AsImplementedInterfaces()
+                    .WithTransientLifetime());
+
+            return builder;
+        }
+
+        public static IJointBuilder AddInMemoryQueryDispatcher(this IJointBuilder builder)
+        {
+            builder.Services.AddSingleton<IQueryDispatcher, QueryDispatcher>();
+            return builder;
+        }
+    }
+}
