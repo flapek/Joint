de4ea9e56ffc8dc33fd810864f9544fe2db74812
flapek <filap4@gmail.com>
2020-10-10 09:31:58 +0200

    add default value

--- src/Joint.Exception/src/Exceptions/DomainException.cs
+++ src/Joint.Exception/src/Exceptions/DomainException.cs
@@ -4,7 +4,7 @@ namespace Joint.Exception.Exceptions
     public abstract class DomainException : System.Exception
     {
         public virtual string Code { get; }
-        public virtual HttpStatusCode StatusCodes { get; }
+        public virtual HttpStatusCode StatusCodes { get; } = HttpStatusCode.BadRequest;
 
         public DomainException(string message) : base(message)
         {
--- src/Joint.Exception/src/Exceptions/InfrastructureException.cs
+++ src/Joint.Exception/src/Exceptions/InfrastructureException.cs
@@ -5,7 +5,7 @@ namespace Joint.Exception.Exceptions
     public abstract class InfrastructureException : System.Exception
     {
         public virtual string Code { get; }
-        public virtual HttpStatusCode StatusCodes { get; }
+        public virtual HttpStatusCode StatusCodes { get; } = HttpStatusCode.InternalServerError;
 
         public InfrastructureException(string message) : base(message)
         {
